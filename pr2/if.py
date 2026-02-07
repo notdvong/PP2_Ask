b2c51b6eb490a110b61a6a84265006e125ba6f17
notdvong <124512861+notdvong@users.noreply.github.com>
2026-02-07 12:15:58 +0500

    Update if.py

---
+++ pr2/if.py
@@ -0,0 +1,29 @@
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+
+# If the condition is true, the code block inside the if statement is executed. If the condition is false, the code block is skipped.
+number = 15
+if number > 0:
+  print("The number is positive")
+
+
+#If statement, without indentation
+a = 33
+b = 200
+if b > a:
+print("b is greater than a") # you will get an error
+
+
+age = 20
+if age >= 18:
+  print("You are an adult")
+  print("You can vote")
+  print("You have full legal rights")
+
+
+is_logged_in = True
+if is_logged_in:
+  print("Welcome back!")

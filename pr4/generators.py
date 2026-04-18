be9e2bfce0629665c5d66f82c6ee1daa24b3b253
notdvong <124512861+notdvong@users.noreply.github.com>
2026-04-18 07:17:08 +0500

    Update generators.py

--- pr4/generators.py
+++ pr4/generators.py
@@ -62,3 +62,39 @@ sq = (x*x for x in range(1, 6))
 for i in sq:
     print(i)
 #generators are also very memory efficient so in large data bases they are really useful because it will take almost no RAM
+
+#1:
+def square_gen(N):
+    for i in range(N + 1):
+        yield i ** 2
+
+for num in square_gen(5):
+    print(num)
+
+#2:
+def even_gen(n):
+    for i in range(0, n + 1, 2):
+        yield str(i)
+
+n = int(input("Enter n: "))
+print(", ".join(even_gen(n)))
+#3:
+def div_gen(n):
+    for i in range(n + 1):
+        if i % 3 == 0 and i % 4 == 0:
+            yield i
+for val in div_gen(100):
+    print(val)
+#4:
+def squares(a, b):
+    for i in range(a, b + 1):
+        yield i * i
+for val in squares(3, 6):
+    print(val)
+#5:
+def countdown(n):
+    while n >= 0:
+        yield n
+        n -= 1
+for num in countdown(5):
+    print(num)

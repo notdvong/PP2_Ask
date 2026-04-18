7aefa4159e6e9635333441e4a4dae2ee5e75137e
notdvong <124512861+notdvong@users.noreply.github.com>
2026-04-18 11:31:22 +0500

    Update enumirate_zip_ex.py

--- pr6/built_in_func/enumirate_zip_ex.py
+++ pr6/built_in_func/enumirate_zip_ex.py
@@ -5,7 +5,6 @@ subjects = ["Discrete Structures", "Calculus 2", "Python"]
 for index, subject in enumerate(subjects):
     print(f"Index {index}: {subject}")
 
-
 #zip: Combines two or more lists so you can loop through them side-by-side
 print("\n--- Zip Example ---")
 students = ["Alice", "Bob", "Charlie"]
